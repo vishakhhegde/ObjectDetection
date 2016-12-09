82a8ab67d714cd05c57f239545cdc604a9439429
vishakhhegde <vishakh@stanford.edu>
2016-12-08 23:33:55 -0800

    tsne activated

--- scripts/training.py
+++ scripts/training.py
@@ -8,6 +8,13 @@ from PIL import Image
 from random import shuffle
 import argparse
 import random
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+
+tsne_switch = False
+tsne_X_array = []
+tsne_colors = []
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -37,7 +44,7 @@ def get_all_paths_and_labels(textFilePath, ImagesDirName):
 	for i in index_shuff:
 		line = allLines[i]
 		line = line.split()
-		ImagePaths.append(os.path.join(MAINFILE_PATH, ImagesDirName, line[0]))
+		ImagePaths.append(os.path.join(args.MAINFILE_PATH, ImagesDirName, line[0]))
 		ImageLabels.append(int(line[1]))
 
 	return ImagePaths, ImageLabels
@@ -79,22 +86,27 @@ positive_batch_size = int((1 - args.background_fraction)*args.batch_size)
 negative_batch_size = args.batch_size - positive_batch_size
 
 for epoch_num in range(args.num_epochs):
+	
 	if args.background_fraction == 1.0:
-		negative_minibatch = random.sample(range(num_negative_images), args.batch_size)		
-		image_inputs = []
-		label_inputs_one_hot = np.zeros((args.batch_size, args.class_count))
-		object_or_not_inputs = []
-		for i, index in enumerate(negative_minibatch):
-			image_input = Image.open(negativeImagePaths[index]).resize((80,80))
-			image_input = image_input.convert('RGB')
-			image_input = np.array(image_input)
-			label_index = negativeImageLabels[index]
+		for i in range(100):
+			negative_minibatch = random.sample(range(num_negative_images), args.batch_size)		
+			image_inputs = []
+			label_inputs_one_hot = np.zeros((args.batch_size, args.class_count))
+			object_or_not_inputs = []
+			for i, index in enumerate(negative_minibatch):
+				image_input = Image.open(negativeImagePaths[index]).resize((80,80))
+				image_input = image_input.convert('RGB')
+				image_input = np.array(image_input)
+				label_index = negativeImageLabels[index]
 
-			image_inputs.append(image_input)
-			label_inputs_one_hot[i, label_index] = 1
-			object_or_not_inputs.append(0)
-		cross_entropy_value, sphere_loss_value, norm_squared_value = sess.run([cross_entropy, sphere_loss, norm_squared], feed_dict = {labelTensor: label_inputs_one_hot, imgTensor: image_inputs, object_or_not: object_or_not_inputs})
-		print object_or_not_inputs, sphere_loss_value, norm_squared_value[0][0]
+				image_inputs.append(image_input)
+				label_inputs_one_hot[i, label_index] = 1
+				object_or_not_inputs.append(0)
+			h_fc1_value, cross_entropy_value, sphere_loss_value, norm_squared_value = sess.run([h_fc1, cross_entropy, sphere_loss, norm_squared], feed_dict = {labelTensor: label_inputs_one_hot, imgTensor: image_inputs, object_or_not: object_or_not_inputs})
+			print ' done with cross entropy loss = ' + str(cross_entropy_value) + ' and with hinge loss = ' + str(sphere_loss_value)
+			print h_fc1_value
+			if tsne_switch:
+				tsne_X_array.append(h_fc1_value[0])
 
 
 	else:
@@ -132,9 +144,30 @@ for epoch_num in range(args.num_epochs):
 
 
 			# train_step.run(feed_dict = {labelTensor: label_inputs_one_hot, imgTensor: image_inputs, object_or_not: object_or_not_inputs})
-			cross_entropy_value, sphere_loss_value, norm_squared_value = sess.run([cross_entropy, sphere_loss, norm_squared], feed_dict = {labelTensor: label_inputs_one_hot, imgTensor: image_inputs, object_or_not: object_or_not_inputs})
-			# print 'batch ' + str(positive_end) + ' done with cross entropy loss = ' + str(cross_entropy_value) + ' and with hinge loss = ' + str(sphere_loss_value)
-			print object_or_not_inputs, sphere_loss_value, norm_squared_value[0][0]
+			h_fc1_value, cross_entropy_value, sphere_loss_value, norm_squared_value = sess.run([h_fc1, cross_entropy, sphere_loss, norm_squared], feed_dict = {labelTensor: label_inputs_one_hot, imgTensor: image_inputs, object_or_not: object_or_not_inputs})
+			print 'batch ' + str(positive_end) + ' done with cross entropy loss = ' + str(cross_entropy_value) + ' and with hinge loss = ' + str(sphere_loss_value)
+			if tsne_switch:
+				tsne_X_array.append(h_fc1_value)
+				tsne_colors.append(object_or_not_inputs)
+
+
+	tsne_X_array = np.array(tsne_X_array)
+	tsne_X_array = np.reshape(tsne_X_array, (-1, 512))
+
+	tsne_colors = np.array(tsne_colors)
+	tsne_colors = np.reshape(tsne_colors, (-1,1))
+
+	print tsne_colors.shape
+	# tsne_X_array = np.reshape()
+	tsne_model = TSNE(n_components=2, random_state=0)
+	tsne_output = tsne_model.fit_transform(tsne_X_array)
+	X_arr = tsne_output[:,0]
+	Y_arr = tsne_output[:,1]
+	# Z_arr = tsne_output[:,2]
+	plt.scatter(X_arr, Y_arr, c = tsne_colors)
+	plt.show()
+
+
 	
 
 	if epoch_num % 10 == 0:
